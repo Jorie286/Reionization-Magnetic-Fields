74b2b5a2cda3ef4141d1cc7dff685cba9992bb3e
Jorie286 <mcdermott.286@osu.edu>
2024-07-05 13:21:01 -0400

    Update Get_Giso_u.py
    Improve readability and functionality. Also, some corrections to variables were made.

--- Get_Giso_u.py
+++ Get_Giso_u.py
@@ -10,59 +10,62 @@ data = np.loadtxt(r'output.txt')
 velocity = np.logspace(1,8,71)
 
 # Compute Giso/u (the coefficient of proportionality) for a specific value of velocity using get_sigmas and get_D_theta.
+def get_n_e(yH, yHe):
+    z = 7
+    Omega_b = 0.046 # Fraction of the universe made of baryonic matter
+    H_o = 2.2618e-18 # Hubble constant
+    G = const.G # gravitational constant
+    n_e = ((3*(1+z)**3*Omega_b*H_o**2)/(8*math.pi*G))*(4.5767e26*(1-yH)+3.6132e25*(1-yHe))
+    return n_e
+
 def get_Giso_u(Te, THII, THeII, yH, yHe, nHtot, k, i):
     k_B = const.k # Boltzmann constant
-    m_e = const.m_e # mass electron (is this what we want here?)
-    z = 7
-    omega_b = 0.046 # Fraction of the universe made of baryonic matter
-    H_o = 2.2682e-18 # Hubble constant
-    G = const.G # gravitational constant
-    n_e = ((3*(1+z)**4*omega_b*H_o)/(8*math.pi*G))*(4.5767e20*(1-yH)+3.6132e19*(1-yHe)) # electron density function
+    m_e = const.m_e # mass electron
+    n_e = get_n_e(yH, yHe) # electron density function
     sigma_e = math.sqrt((k_B**2*Te)/(m_e**2))
     
     Giso_const = (1/n_e)*((4*math.sqrt(math.pi))/math.sqrt(6))
-    Giso = velocity[i]**2*get_sigmas(20, get_D_theta(5e4, THII, THeII, yH, yHe, i))[0]*((n_e*velocity[i])/((2*math.pi)**(3/2)*sigma_e**5))*math.exp(-(velocity[i]**2)/(2*sigma_e**2))
+    Giso = velocity[i]**2*get_sigmas(20, get_D_theta(5e4, Te, THII, THeII, yH, yHe, i))[0]*((n_e*velocity[i])/((2*math.pi)**(3/2)*sigma_e**5))*math.exp(-(velocity[i]**2)/(2*sigma_e**2))
     Giso_u = Giso_const*Giso
     return Giso_u
 
 # Computing D_theta
-def get_D_theta(T, THII, THeII, yH, yHe, i):
+def get_D_theta(T, Te, THII, THeII, yH, yHe, i):
     k_B = const.k # Boltzmann constant
-    R_y = const.Rydberg # Rydberg constant (unit of energy)
+    R_y = const.Rydberg*const.h # Rydberg constant (unit of energy)
     a_o = 5.29177210903e-11 # Bohr radius
-    m_a = const.m_e # mass of an electron (is this what we want here?)
+    m_a = const.m_e # mass of an electron
     m_b1 = const.m_p # mass of HII
     m_b2 = 2*const.m_p+2*const.m_n+const.m_e # mass of HeII (ionized once so it still has one electron)
-    q_a = -const.eV # charge of an electron (is this what we want here?)
+    m_b3 = const.m_e # mass of an electron
+    q_a = -const.eV # charge of an electron (also the charge of m_b3)
     q_b = const.eV # charge of HII and HeII
     epsilon_o = const.epsilon_0 # vacuum permittivity
-    z = 7
-    omega_b = 0.046 # Fraction of the universe made of baryonic matter
+    Omega_b = 0.046 # Fraction of the universe made of baryonic matter during reionization
     H_o = 2.2618e-18 # Hubble constant
     G = const.G # gravitational constant
-    n_e = ((3*(1+z)**4*omega_b*H_o)/(8*math.pi*G))*(4.5767e20*(1-yH)+3.6132e19*(1-yHe)) # electron density function
-    n_b1 = ((3*(1+z)**4*omega_b*H_o)/(8*math.pi*G))*4.5767e20*(1-yH) # number density of ionized H
-    n_b2 = ((3*(1+z)**4*omega_b*H_o)/(8*math.pi*G))*3.6132e19*(1-yHe) # number density of ionized He
-    # Calculate the velocity dispersion (one for each of the species)
-    sigma_b1 = math.sqrt((k_B**2*THII)/(m_b1**2))
-    sigma_b2 = math.sqrt((k_B**2*THeII)/(m_b2**2))
+    z = 7
+    n_e = get_n_e(yH, yHe) # electron density function (also the number density of m_b3)
+    n_b1 = ((3*(1+z)**3*Omega_b*H_o**2)/(8*math.pi*G))*4.5767e26*(1-yH) # number density of ionized H
+    n_b2 = ((3*(1+z)**3*Omega_b*H_o**2)/(8*math.pi*G))*3.6132e25*(1-yHe) # number density of ionized He
+    n_b3 = n_e
     # Calculate the columb logarithm.
     lamda_c = ((3/2)*math.log((k_B*T)/R_y))-((1/2)*math.log(64*math.pi*a_o**3*n_e))
     
-    # Calculate the first portion of D_theta for HII.
-    D_one_1 = (q_a**2*q_b**2*n_b1*lamda_c)/(8*math.pi*epsilon_o**2*m_a**2*velocity[i]**3)
-    # Calculate the second portion of D_theta for HII.
-    D_two_1 = (1-(sigma_b1**2/velocity[i]**2))*math.erf(velocity[i]/(math.sqrt(2)*sigma_b1))+math.sqrt(2/math.pi)*(sigma_b1/velocity[i])*math.exp(-velocity[i]**2/(2*sigma_b1**2))
+    # Calculate the velocity dispersion (one for each of the species)
+    sigma_b1 = math.sqrt((k_B**2*THII)/(m_b1**2))
+    sigma_b2 = math.sqrt((k_B**2*THeII)/(m_b2**2))
+    sigma_b3 = math.sqrt((k_B**2*Te)/(m_b3**2))
     
-    # Calculate the first portion of D_theta for HeII.
-    D_one_2 = (q_a**2*q_b**2*n_b2*lamda_c)/(8*math.pi*epsilon_o**2*m_a**2*velocity[i]**3)
-    # Calculate the second portion of D_theta for HeII.
-    D_two_2 = (1-(sigma_b2**2/velocity[i]**2))*math.erf(velocity[i]/(math.sqrt(2)*sigma_b2))+math.sqrt(2/math.pi)*(sigma_b2/velocity[i])*math.exp(-velocity[i]**2/(2*sigma_b2**2))
-    
-    D_final = D_one_1*D_two_1+D_one_2*D_two_2
+    numbers = [n_b1, n_b2, n_b3, sigma_b1, sigma_b2, sigma_b3] # List of coefficients to be used in calculating D_theta.
+    D_final = 0
+    for n in range(0,3): # Iterate through numbers and calculate D_theta for each of the species. Returns the sum over all species.
+        D_one = (q_a**2*q_b**2*numbers[0+n]*lamda_c)/(8*math.pi*epsilon_o**2*m_a**2*velocity[i]**3)
+        D_two = (1-(numbers[3+n]**2/velocity[i]**2))*math.erf(velocity[i]/(math.sqrt(2)*numbers[3+n]))+math.sqrt(2/math.pi)*(numbers[3+n]/velocity[i])*math.exp(-velocity[i]**2/(2*numbers[3+n]**2))
+        D_final = D_one*D_two+D_final
     return D_final
 
-def get_sigmas(n,c): # m=1, n=number sigma parameters to be solved for, c=iD_theta/kv
+def get_sigmas(n, c): # m=1, n=number sigma parameters to be solved for, c=iD_theta/kv
     
     # Create a zero matrix and fill it with the diagonal part of the tridiagonal matrix
     ab = np.zeros((3,n), dtype = np.complex128)
