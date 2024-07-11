be8532cdf1cd44720d405ee77fd068d62e408798
Jorie286 <mcdermott.286@osu.edu>
2024-07-11 14:23:30 -0400

    Update Graphing_Giso_u.py
    Improve the efficiency of the graphing code.

--- Giso_u/Graphing_Giso_u.py
+++ Giso_u/Graphing_Giso_u.py
@@ -10,45 +10,15 @@ from Get_Giso_u import data
 # Set up a 2x3 grid of subplots so all plots can be viewed together.
 fig, axs = plt.subplots(nrows = 2, ncols = 3, figsize=(14,10))
 
+# Create lists to iterate through while creating the plots. This eliminates many lines of uncessary code.
+positions = [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2)]
+data_list = [data[:,0]*12000, data[:,2], data[:,3], data[:,5], data[:,7], data[:,13]]
+labels = ["Time", "yH", "yHe", "T e", "THII", "THeII"] 
 # Graph real and imaginary portions of Giso_list seperatly for each graph.
 # Plot Giso_u over time.
-axs[0,0].plot(data[:,0]*12000, real, color = "c", label = "Giso real") # Note: the timestep for data is 12000 (years???).
-axs[0,0].plot(data[:,0]*12000, imaginary, color = "r", label = "Giso imaginary")
-axs[0,0].set_xlabel("Time")
-axs[0,0].set_ylabel("Giso/u")
-axs[0,0].legend()
-
-# Plot Giso_u against yH (neutral H fraction).
-axs[0,1].plot(data[:,2], real, color = "c", label = "Giso real")
-axs[0,1].plot(data[:,2], imaginary, color = "r", label = "Giso imaginary")
-axs[0,1].set_xlabel("yH")
-axs[0,1].set_ylabel("Giso/u")
-axs[0,1].legend()
-
-# Plot Giso_u against yHe (neutral He fraction).
-axs[0,2].plot(data[:,3], real, color = "c", label = "Giso real")
-axs[0,2].plot(data[:,3], imaginary, color = "r", label = "Giso imaginary")
-axs[0,2].set_xlabel("yHe")
-axs[0,2].set_ylabel("Giso/u")
-axs[0,2].legend()
-
-# Plot Giso_u against Te.
-axs[1,0].plot(data[:,5], real, color = "c", label = "Giso real")
-axs[1,0].plot(data[:,5], imaginary, color = "r", label = "Giso imaginary")
-axs[1,0].set_xlabel("T e")
-axs[1,0].set_ylabel("Giso/u")
-axs[1,0].legend()
-
-# Plot Giso_u against THII.
-axs[1,1].plot(data[:,7], real, color = "c", label = "Giso real")
-axs[1,1].plot(data[:,7], imaginary, color = "r", label = "Giso imaginary")
-axs[1,1].set_xlabel("THII")
-axs[1,1].set_ylabel("Giso/u")
-axs[1,1].legend()
-
-#Plot Giso_u against THeII.
-axs[1,2].plot(data[:,13], real, color = "c", label = "Giso real")
-axs[1,2].plot(data[:,13], imaginary, color = "r", label = "Giso imaginary")
-axs[1,2].set_xlabel("THeII")
-axs[1,2].set_ylabel("Giso/u")
-axs[1,2].legend()
+for g in range(0,6):
+    axs[positions[0+g]].plot(data_list[0+g], real, color = "c", label = "Giso real")  # Note: the timestep for data is 12000 (years???).
+    axs[positions[0+g]].plot(data_list[0+g], imaginary, color = "r", label = "Giso imaginary")
+    axs[positions[0+g]].set_xlabel(labels[0+g])
+    axs[positions[0+g]].set_ylabel("Giso/u")
+    axs[positions[0+g]].legend()
