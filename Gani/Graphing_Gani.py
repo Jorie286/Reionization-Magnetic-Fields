5e068f3960edc5a9d9b2127d89fa028a8e83b692
Jorie286 <mcdermott.286@osu.edu>
2024-11-30 16:20:53 -0500

    Update Graphing_Gani.py
    Update code to include k slabs when graphing.

--- Gani/Graphing_Gani.py
+++ Gani/Graphing_Gani.py
@@ -6,51 +6,6 @@ import numpy as np
 from Get_Gani import Gani_data
 from Get_Gani import data
 
-# plot the source term values at various bin numbers, we want to find where it reaches its maximum
-# get the S_{2,0} data from the file created by Get_Gani
-S20 = np.loadtxt(r'S20.txt')
-# create a plot and indexing list for convenient graphing
-fig, ax = plt.subplots(figsize=(14,10))
-v_list = []
-for i in range(0,71):
-    v_list.append(i)
-# plot source term over various slab numbers (the numbers plotted below correspond to where S_{2,0} reaches a max)
-ax.plot(velocity[v_list], (4*np.pi*np.array(velocity[v_list])**3*S20[(71*999):(71*1000)]), label = "Slab 1000")
-ax.plot(velocity[v_list], (4*np.pi*np.array(velocity[v_list])**3*S20[(71*1029):(71*1030)]), label = "Slab 1030")
-ax.plot(velocity[v_list], (4*np.pi*np.array(velocity[v_list])**3*S20[(71*1049):(71*1050)]), label = "Slab 1050")
-ax.plot(velocity[v_list], (4*np.pi*np.array(velocity[v_list])**3*S20[(71*1069):(71*1070)]), label = "Slab 1070")
-ax.plot(velocity[v_list], (4*np.pi*np.array(velocity[v_list])**3*S20[(71*1089):(71*1090)]), label = "Slab 1090")
-ax.plot(velocity[v_list], (4*np.pi*np.array(velocity[v_list])**3*S20[(71*1099):(71*1100)]), label = "Slab 1100")
-# set up plot labels
-ax.set_xlabel("Velocity [m/s]")
-ax.set_ylabel("Source Term Value")
-ax.set_title("Change in Source Term for a Selection of Slab Numbers")
-ax.legend()
-fig.show()
-
-# plot the multipole moment values of a_{2,0} over varous slab numbers like we plotted S_{2,0} above
-# get the a_{2,0} data file from the output of Get_Gani
-a20 = np.loadtxt(r'a20.txt')
-# create a plot and indexing list for convenient graphing
-fig, ax = plt.subplots(figsize=(14,10))
-v_list = []
-for i in range(0,71):
-    v_list.append(i)
-# plot multipole moment over various slab numbers
-ax.plot(velocity[v_list], (4*np.pi*velocity[v_list]**3*a20[(71*999):(71*1000)]), label = "Slab 1000")
-ax.plot(velocity[v_list], (4*np.pi*velocity[v_list]**3*a20[(71*1029):(71*1030)]), label = "Slab 1030")
-ax.plot(velocity[v_list], (4*np.pi*velocity[v_list]**3*a20[(71*1049):(71*1050)]), label = "Slab 1050")
-ax.plot(velocity[v_list], (4*np.pi*velocity[v_list]**3*a20[(71*1069):(71*1070)]), label = "Slab 1070")
-ax.plot(velocity[v_list], (4*np.pi*velocity[v_list]**3*a20[(71*1089):(71*1090)]), label = "Slab 1090")
-ax.plot(velocity[v_list], (4*np.pi*velocity[v_list]**3*a20[(71*1099):(71*1100)]), label = "Slab 1100")
-ax.plot(velocity[v_list], (4*np.pi*velocity[v_list]**3*a20[(71*1199):(71*1200)]), label = "Slab 1200")
-# set up the plot labels
-ax.set_xlabel("Velocity [m/s]")
-ax.set_ylabel("Multipole Moment Value")
-ax.set_title("Change in Multipole Moment for a Selection of Slab Numbers")
-ax.legend()
-fig.show()
-
 # iterate through all the values of a20 and find the maximum absolute value of each slab
 n = 0
 v = 1
@@ -60,7 +15,7 @@ max_val = 0
 a_list = []
 a_max = []
 for j in a20:
-    if n==71*v: # check to make sure we are not at the end of a slab
+    if n==71*8*v: # check to make sure we are not at the end of a slab
         max_a = np.max(np.abs(a_list))
         # get the slab and value of a_20 at its maximum
         if max_a>max_val:
@@ -73,10 +28,10 @@ for j in a20:
     # add the value of a20 for the velocity bin we are on
     a_list.append(j)
     n+=1
-    if n==2000*71:
+    if n==2000*8*71:
         a_max.append(np.max(np.abs(a_list)))
 print("The maximum absolute value of the multipole moment is", max_val)
-print("The slab number where the absolute value of the multipole moment is at a maximum is", max_slab)
+print("The slab number (j) where the absolute value of the multipole moment is at a maximum is", max_slab)
 
 # iterate through all the values of S20 and find the maximum absolute value of each slab
 n = 0
@@ -87,7 +42,7 @@ max_val_S = 0
 S_list = []
 S_max = []
 for j in S20:
-    if n==71*v: # check to make sure we are not at the end of a slab
+    if n==71*8*v: # check to make sure we are not at the end of a slab
         max_S = np.max(np.abs(S_list))
         # get the slab and value of S_20 at its maximum
         if max_S>max_val_S:
@@ -100,11 +55,56 @@ for j in S20:
     # add the value of a20 for the velocity bin we are on
     S_list.append(j)
     n+=1
-    if n==2000*71:
+    if n==2000*8*71:
         S_max.append(np.max(np.abs(S_list)))
 print("The maximum absolute value of the source term is", max_val_S)
-print("The slab number where the absolute value of the source term is at a maximum is", max_slab_S)
-        
+print("The slab number (j) where the absolute value of the source term is at a maximum is", max_slab_S)
+
+
+# plot the source term values at various bin numbers, we want to find where it reaches its maximum
+# get the S_{2,0} data from the file created by Get_Gani
+S20 = np.loadtxt(r'S20.txt')
+# create a plot and indexing list for convenient graphing
+fig, ax = plt.subplots(figsize=(14,10))
+# plot source term over various slab numbers
+# Note: we are only plotting the last k slab
+ax.plot(velocity, (4*np.pi*np.array(velocity)**3*S20[(71*8*999):(71*8*1000)][71*7:71*8]), label = "Slab 1000")
+ax.plot(velocity, (4*np.pi*np.array(velocity)**3*S20[(71*8*1029):(71*8*1030)][71*7:71*8]), label = "Slab 1030")
+ax.plot(velocity, (4*np.pi*np.array(velocity)**3*S20[(71*8*1049):(71*8*1050)][71*7:71*8]), label = "Slab 1050")
+ax.plot(velocity, (4*np.pi*np.array(velocity)**3*S20[(71*8*1069):(71*8*1070)][71*7:71*8]), label = "Slab 1070")
+ax.plot(velocity, (4*np.pi*np.array(velocity)**3*S20[(71*8*1089):(71*8*1090)][71*7:71*8]), label = "Slab 1090")
+ax.plot(velocity, (4*np.pi*np.array(velocity)**3*S20[(71*8*1099):(71*8*1100)][71*7:71*8]), label = "Slab 1100")
+# set up plot labels
+ax.set_xlabel("Velocity [m/s]")
+ax.set_ylabel("Source Term Value")
+ax.set_title("Change in Source Term for a Selection of Slab Numbers")
+ax.legend()
+fig.show()
+
+
+# plot the multipole moment values of a_{2,0} over varous slab numbers like we plotted S_{2,0} above
+# get the a_{2,0} data file from the output of Get_Gani
+a20 = np.loadtxt(r'a20.txt')
+print(len(a20))
+# create a plot and indexing list for convenient graphing
+fig, ax = plt.subplots(figsize=(14,10))
+# plot multipole moment over various slab numbers
+# Note: we are only plotting the last k slab
+ax.plot(velocity, (4*np.pi*velocity**3*a20[(71*8*999):(71*8*1000)][71*7:71*8]), label = "Slab 1000")
+ax.plot(velocity, (4*np.pi*velocity**3*a20[(71*8*1029):(71*8*1030)][71*7:71*8]), label = "Slab 1030")
+ax.plot(velocity, (4*np.pi*velocity**3*a20[(71*8*1049):(71*8*1050)][71*7:71*8]), label = "Slab 1050")
+ax.plot(velocity, (4*np.pi*velocity**3*a20[(71*8*1069):(71*8*1070)][71*7:71*8]), label = "Slab 1070")
+ax.plot(velocity, (4*np.pi*velocity**3*a20[(71*8*1089):(71*8*1090)][71*7:71*8]), label = "Slab 1090")
+ax.plot(velocity, (4*np.pi*velocity**3*a20[(71*8*1099):(71*8*1100)][71*7:71*8]), label = "Slab 1100")
+ax.plot(velocity, (4*np.pi*velocity**3*a20[(71*8*1199):(71*8*1200)][71*7:71*8]), label = "Slab 1200")
+# set up the plot labels
+ax.set_xlabel("Velocity [m/s]")
+ax.set_ylabel("Multipole Moment Value")
+ax.set_title("Change in Multipole Moment for a Selection of Slab Numbers")
+ax.legend()
+fig.show()
+
+
 # plot the values of Gani against the neutal fractions
 # set up a pair of subplots that are ligned up so that we can compare neutral fractions of hydrogen and helium to change in Gani
 fig, axs = plt.subplots(2, figsize=(14,10))
@@ -113,7 +113,7 @@ j_list = []
 for i in range(0,2000):
     j_list.append(i)
 # plot Gani over bins (j)
-axs[0].plot(j_list, Gani_data, label="Gani")
+axs[0].plot(j_list, Gani_data[::8], label="Gani")
 # plot the hydrogen and helium neutral fraction over the bins (j) in a separate subplot from Gani
 axs[1].plot(j_list, data[:,2], label="yH")
 axs[1].plot(j_list, data[:,3], label="yHe")
@@ -131,6 +131,7 @@ axs[1].set_ylabel("Neutral Fraction")
 axs[0].set_title("Gani Compared to the Neutral Fraction for all Slab Numbers")
 fig.show()
 
+
 # make a plot of a_{2,0} at its largest (absolute) value
 # find the velocity bin number of the following value
 v_n = ((3*k_B*data[1077,5])/m_e)**0.5
@@ -138,13 +139,10 @@ for v in range(len(velocity)):
     if v_n <= velocity[v]:
         v_bin = v
         break
-        
 fig, ax = plt.subplots(figsize=(14,10))
-v_list = []
-for i in range(0,71):
-    v_list.append(i)
 # plot multipole moment at its maximum
-ax.plot(velocity[v_list], (4*np.pi*velocity[v_list]**3*a20[(71*1076):(71*1077)]), label = "Slab 1077")
+# Note: we are only plotting the last k bin
+ax.plot(velocity, (4*np.pi*velocity**3*a20[(71*8*1076):(71*8*1077)][71*7:71*8]), label = "Slab 1077")
 ax.axvline(x=velocity[v_bin], color = 'r', linestyle = "--")
 # add the equation for this and arrow for this value on the plot
 equation = r'$\sqrt{\frac{3 k_B T_e}{m_e}}$'
@@ -156,13 +154,11 @@ ax.set_title("Change in Multipole Moment at Maximum")
 ax.legend()
 fig.show()
 
+
 # make a plot of S_{2,0} at its largest (absolute) value
 fig, ax = plt.subplots(figsize=(14,10))
-v_list = []
-for i in range(0,71):
-    v_list.append(i)
 # plot multipole moment at its maximum
-ax.plot(velocity[v_list], (4*np.pi*np.array(velocity[v_list])**3*S20[(71*1068):(71*1069)]), label = "Slab 1069")
+ax.plot(velocity, (4*np.pi*np.array(velocity)**3*S20[(71*8*1068):(71*8*1069)][71*7:71*8]), label = "Slab 1069")
 # set up the plot labels
 ax.set_xlabel("Velocity [m/s]")
 ax.set_ylabel("Source Term Value")
