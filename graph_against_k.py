734fa8de3b6c32dc13a13e6e1e5b2f772c531a19
Jorie286 <mcdermott.286@osu.edu>
2025-01-06 21:42:40 -0500

    Update graph_against_k.py
    Change indexing and variables to match with calc_params.py.

--- graph_against_k.py
+++ graph_against_k.py
@@ -6,12 +6,7 @@ from Giso_u.Get_Giso_u import imaginary
 # Get the data from Get_Gani to be used for graphing.
 from Gani.Get_Gani import Gani_data
 from Gani.Get_Gani import data
-
-# get constants for calculating the electron number denstiy
-G = const.G # gravitational constant
-z = 7 # redshift
-Omega_b = 0.046 # Fraction of the universe made of baryonic matter during reionization
-H_o = 2.2618e-18 # Hubble constant
+import calc_params
 
 # Compute the electron number density during reionization.
 def get_n_e(yH, yHe):
@@ -28,34 +23,33 @@ def get_n_e(yH, yHe):
     Returns
         the number density of electrons under the given conditions
         
-    Date of last revision: October 28, 2024
+    Date of last revision: January 6, 2025
     """
-    n_e = ((3*(1+z)**3*Omega_b*H_o**2)/(8*math.pi*G))*(4.5767e26*(1-yH)+3.6132e25*(1-yHe))
+    n_e = ((3*(1+calc_params.z)**3*calc_params.Omega_b*calc_params.H_o**2)/(8*math.pi*calc_params.G))*(calc_params.h*(1-yH)+calc_params.he*(1-yHe))
     return n_e
-
-# calculate the values of the imaginary frequency for all slabs and wavenumbers
+    
 im_w_list = []
 mu_0 = (4*np.pi)*(10**(-7)) # permiablity of free space (units of newtons ampere**-2)
-for m in range(2000):
+for m in range(0, calc_params.NSLAB):
     n_e = get_n_e(data[m,2], data[m,3])
-    k_sd = np.sqrt((mu_0*n_e*q_a**2)/m_b3)
-    for n in range(8):
-        im_w_list.append((k[n*10]/Giso_im_data[m*n])*(Gani_data[m*n]-(k[n*10]/k_sd)**2))
-
+    k_sd = np.sqrt((mu_0*n_e*calc_params.q_a**2)/calc_params.m_e)
+    for n in range(0, calc_params.num_k):
+        im_w_list.append((calc_params.k[n*calc_params.k_step]/imaginary[m*n])*(Gani_data[m*n]-(calc_params.k[n*calc_params.k_step]/k_sd)**2))
+print(im_w_list)
+Im_w_arr = np.array(im_w_list).reshape(calc_params.NSLAB,calc_params.num_k) # reshape the list for easier graphing and analysis
 
 # make a plot of imaginary w against k
 fig, ax = plt.subplots(figsize=(14,10))
-ax.plot(k[:8], im_w_list[::2000])
+ax.plot(calc_params.k[::calc_params.k_step], Im_w_arr[0,:]) # plot the first row if values for imaginary omega.
 ax.set_title("Imaginary w against k")
 ax.set_xlabel("k")
 ax.set_ylabel("Imaginary w")
 fig.show()
 
-
 # plot the wavenumbers agains the values that we calculated in Get_Giso and Get_Gani
 # make a plot of Giso against k
 fig, ax = plt.subplots(figsize=(14,10))
-ax.plot(k[::10][:8], Giso_im_data[(8*1069):(8*1070)], label = "k slab 1070")
+ax.plot(calc_params.k[::calc_params.k_step], imaginary[(calc_params.num_k*1069):(calc_params.num_k*1070)], label = "k slab 1070")
 ax.set_title("Imaginary Giso against k")
 ax.set_xlabel("k")
 ax.set_ylabel("Giso Imaginary Value")
@@ -65,7 +59,7 @@ fig.show()
 
 # make a plot of Gani against k
 fig, ax = plt.subplots(figsize=(14,10))
-ax.plot(k[::10][:8], Gani_data[(8*1069):(8*1070)], label = "k slab 1070")
+ax.plot(calc_params.k[::calc_params.k_step], Gani_data[(calc_params.num_k*1069):(calc_params.num_k*1070)], label = "k slab 1070")
 ax.set_title("Gani against k")
 ax.set_xlabel("k")
 ax.set_ylabel("Gani Value")
