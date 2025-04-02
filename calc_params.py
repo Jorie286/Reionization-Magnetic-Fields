5274d45588bb7a5597d28bcb45a5bc09ef8dc5f8
Jorie286 <mcdermott.286@osu.edu>
2025-04-01 21:19:01 -0400

    Update calc_params.py
    Increase the number of velocity bins and nu bins to improve the smoothness and accuracy of the results.

--- calc_params.py
+++ calc_params.py
@@ -3,7 +3,7 @@ import scipy.constants as const
 
 # define the maximum velocity and number of velocities that we want to use for the compuation
 vmax = 5.0e6
-Nv = 71
+Nv = 142
 # Create a distribution of velocities in linear space.
 velocity = np.linspace(vmax/Nv, vmax, Nv)
 delta_v = (vmax-(vmax/Nv))/Nv # get the change in v of each step in the velocity data
@@ -47,7 +47,7 @@ I_H = 13.59*const.eV
 I_He = 24.687*const.eV
 
 DNHI = 2.5e20 # width of a grid cell within the reionization front model
-N_NU = 128 # number of frequency bins
+N_NU = 512 # number of frequency bins
 Timestep = 12000 # timestep from the inital simulation where we gathered the data
 NSLAB = 2000 # define the number of slabs we want to use in the calculaiton
 NHtot = 200 # total number of hydrogen atoms in the distribution
